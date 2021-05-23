0b94866bb77e749b99ffbc65b62c9255c07db7ec
Acla Putra <aclalead@gmail.com>
2021-05-23 07:32:54 +0700

    connect client to backend

--- server.go
+++ server.go
@@ -3,11 +3,37 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-  app := fiber.New()
+	app := fiber.New()
 
-  app.Get("/", func(c *fiber.Ctx) error {
-    return c.SendString("Hello, World!")
-  })
+	app.Static("/", "./public") 
 
-  app.Listen(":3000")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("./public/index.html")
+	})	
+
+	app.Get("/:name?", func(c *fiber.Ctx) error {
+		if c.Params("name") != "" {
+		return c.SendString("Hello " + c.Params("name"))
+		// => Hello john
+		}
+		return c.SendString("Where is john?")
+	})
+
+	// GET http://localhost:3000/api/user/john
+
+	app.Get("/api/*", func(c *fiber.Ctx) error {
+		return c.SendString("API path: " + c.Params("*"))
+		// => API path: user/john
+	})
+
+	// GET http://localhost:8080/hello%20world
+
+		// app.Get("/:value", func(c *fiber.Ctx) error {
+		// 	return c.SendString("value: " + c.Params("value"))
+		// 	// => Get request with value: hello world
+		// })
+
+		// GET http://localhost:3000/john
+
+	app.Listen(":3000")
 }
